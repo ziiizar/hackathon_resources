7d1b2a8d6c68892b7b869b56d951ea02432c08a9
ziiizar <cesarfpna@gmail.com>
2025-02-21 20:11:55 +0000

    fix 404 on resource when refresh

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,10 @@ if (process.env.TEMPO === "true") {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+  base:
+    process.env.NODE_ENV === "development"
+      ? "/"
+      : process.env.VITE_BASE_PATH || "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
@@ -31,5 +34,6 @@ export default defineConfig({
   server: {
     // @ts-ignore
     allowedHosts: true,
-  }
+    historyApiFallback: true,
+  },
 });
